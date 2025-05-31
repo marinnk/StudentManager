d624ece1fa94e6dc17152b232b6d1e15cae62721
marinnk <marinnobaka@yahoo.co.jp>
2025-05-31 22:39:57 +0900

    依存関係追加

--- src/main/java/raisetech/Student/Manager/StudentManagerApplication.java
+++ src/main/java/raisetech/Student/Manager/StudentManagerApplication.java
@@ -1,5 +1,6 @@
 package raisetech.Student.Manager;
 
+import io.micrometer.common.util.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
