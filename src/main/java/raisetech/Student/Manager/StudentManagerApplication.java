e7df1ec0c7af8d8a4cc0de7369dc5b39783a3d44
marinnk <marinnobaka@yahoo.co.jp>
2025-06-29 22:15:10 +0900

    studentsの全件取得

--- src/main/java/raisetech/Student/Manager/StudentManagerApplication.java
+++ src/main/java/raisetech/Student/Manager/StudentManagerApplication.java
@@ -1,15 +1,11 @@
 package raisetech.Student.Manager;
 
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -29,40 +25,9 @@ public class StudentManagerApplication {
 	}
 
 	//登録されたデータを表示する
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-		if(student == null) {
-			return name+ " さんは登録されていません。";
-		}
-		return  student.getName() + "：" + student.getAge() + "歳";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 	}
 
-	//新規登録
-	@PostMapping("/student")
-	public String registerStudent(@RequestParam String name,@RequestParam int age) {
-		name = name.trim();
-
-		repository.registerStudent(name, age);
-		return name + " " + age + "歳を登録";
-
-	}
-
-
-	//情報の更新
-	@PatchMapping("/student")
-	public String updateStudentName(@RequestParam String name, @RequestParam int age) {
-		name = name.trim();
-		repository.updateStudent(name,age);
-
-		return name + "を" + age + "歳に更新";
-	}
-
-	//データの削除
-	@DeleteMapping("/student")
-	public String student(@RequestParam String name) {
-		name = name.trim();
-		repository.deleteStudent(name);
-		return name + "を" + "削除";
-	}
 }
